315a84853952dc54270c5e0984bace977c4b3552
asd <sd>
2017-02-01 14:11:08 +0800

    update profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 
     html,body,.profile-image{
         height: 100%;
-        font-family: 'Poppins', arial, sans-serif;
+        font-family: 'Poppins', sans-serif;
         display: block;
         color: white;
 
@@ -11,14 +11,16 @@
     .profile-image{
         /*background: url('/assets/images/aa.png') no-repeat center;*/
         /*background-size: contain;*/
-        background: #000000;
+        background: #111111;
     }
 
     .container{
         height: 100%;
         display: table;
-        clear: both;
+        width: 100%;
         padding-bottom: 0px;
+        margin: 0;
+        padding: 0 50px;
     }
 
     .row{
@@ -88,6 +90,7 @@
     .navbar{
         margin-bottom: 0px;
         border-radius: 0;
+        z-index: 1000;
     }
 
     .navbar .container{
@@ -111,26 +114,100 @@
     }
 
     #about{
-        padding: 650px 0 0 0;
-        background: black;
+        padding-top: 80px;
+        background: #fdfdfe;
+        color: #5a5252;
     }
-    #homers{
-         padding: 650px 0 0 0;
-         background: green;
+    #about h2 ,#introduction h2 ,#skills h2{
+        color: #5a5252;
+    }
+    #about .container .row{
+        display: block;
+    }
+
+    #introduction{
+        padding: 52px 0px;
+        border-bottom: 1px solid #F1EBEB;
+        color: #5a5252;
+    }
+
+    #introduction .container .row{
+        display: block;
+    }
+
+    #introduction .profile-image{
+        height: 200px;
+        width: auto;
+        height: 300px;
+        width: 80%;
+
+    }
+    #introduction ul{
+        padding: 0 0 0 20px;
+        margin: 0;
+    }
+    #introduction a{
+        background-color: #111111;
+        border-radius: 1px;
+        color: #ffffff !important;
+        display: inline-block;
+        font-size: 13px;
+        letter-spacing: 1px;
+        margin: 14px 0;
+        padding: 10px 30px;
+        transition: all 0.3s ease;
+    }
+
+
+    #skills{
+         padding: 80px 0 0 0;
+        color: #5a5252;
      }
-    #sample{
-        padding: 650px 0 0 0;
-        background: red;
+
+    #skills-1{
+        padding: 80px 0;
+        color: #5a5252;
+        border-bottom: 1px solid #F1EBEB;
     }
+    
+    #portfolio{
+        padding: 80px 0 0 0;
+    }
+    #portfolio .container .row{
+        display: block;
+    }
+
+    #portfolio .portfolio-list{
+        display: block;
+    }
+    
+    #portfolio .portfolio-list .portfolio-items{
+        display: inline-block;
+        padding: 13px;
+
+    }
+    .portfolio-items img{
+        height: 200px;
+        width: 290px;
+    }
+    
 </style>
 <!--HEADER-->
-<div class="profile-image" id="top" data-spy="scroll">
+<div class="profile-image" id="top" data-spy="scroll" >
     <div class="header-nav col-md-12">
         <ul>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
+            <li>
+                <a class="" href="#about">Lorem</a>
+            </li>
+            <li>
+                <a class="page-scroll" href="#homers">Lorem</a>
+            </li>
+            <li>
+                <a class="page-scroll" href="#sample">Lorem</a>
+            </li>
+            <li>
+                <a class="page-scroll" href="#sample">Lorem</a>
+            </li>
         </ul>
     </div>
     <div class="col-md-12 profile-body">
@@ -153,7 +230,7 @@
 </div>
 <!--END OF HEADER-->
 <div id="navigation" style="height: 50px">
-    <nav class="navbar navbar-default" data-spy="affix" data-offset-top="650">
+    <nav class="navbar navbar-default" data-spy="affix" data-offset-top="640">
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -169,17 +246,16 @@
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a class="page-scroll" href="#about">Home</a>
+                        <a class="page-scroll" href="#about">Lorem</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#homers">Blog</a>
+                        <a class="page-scroll" href="#skills">Lorem</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#sample">About</a>
+                        <a class="page-scroll" href="#skills-1">Lorem</a>
                     </li>
-                    <li>
-                        <a>Sign in</a>
-                    </li>
+
+
 
                 </ul>
             </div>
@@ -188,31 +264,167 @@
 </div>
 <section id="about">
     <div class="container">
-        
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Things I do.</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <h2><span>1.</span> Things I do.</h2>
+                <p>Lorem ipsum lorem ipsum</p>
+            </div>
+            <div class="col-md-3">
+                <h2><span>2.</span>Things I do.</h2>
+                <p>Lorem ipsum lorem ipsum</p>
+            </div>
+            <div class="col-md-3">
+                <h2><span>3.</span>Things I do.</h2>
+                <p>Lorem ipsum lorem ipsum</p>
+            </div>
+            <div class="col-md-3">
+                <h2><span>4.</span>Things I do.</h2>
+                <p>Lorem ipsum lorem ipsum</p>
+            </div>
+        </div>
     </div>
 </section>
-<section id="homers">
+<section id="introduction">
     <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img class="profile-image" src="../assets/images/background.jpg">
+            </div>
+            <div class="col-md-6">
 
+                <h2>Introduction.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel volutpat nunc, ac tristique sem. Mauris ut est nisi. Sed gravida nulla in venenatis aliquam. Sed ipsum augue, egestas et tellus in, mollis accumsan ex.</p>
+                <ul>
+                    <li>Date of Birth: 04 June, 1990</li>
+                    <li>Country: Philippines</li>
+                </ul>
+                <div><a href="#">Lorem ipsum</a></div>
+            </div>
+        </div>
     </div>
 </section>
-<section id="sample">
+<section id="skills">
     <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Skills.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel volutpat nunc, ac tristique sem. Mauris ut est nisi. Sed gravida nulla in venenatis aliquam. Sed ipsum augue, egestas et tellus in, mollis accumsan ex.</p>
 
+            </div>
+            <div class="col-md-4">
+                <h2>Back-end</h2>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h2>Front-end</h2>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </section>
 
+<section id="skills-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Skills.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel volutpat nunc, ac tristique sem. Mauris ut est nisi. Sed gravida nulla in venenatis aliquam. Sed ipsum augue, egestas et tellus in, mollis accumsan ex.</p>
+
+            </div>
+            <div class="col-md-4">
+                <h2>Back-end</h2>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h2>Front-end</h2>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="portfolio">
+
+    <div class="container">
+        <div class="row">
+            <h2>Portfolio</h2>
+        </div>
+        <div class="row">
+            <div class="portfolio-list">
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="portfolio-items">
+                    <img src="../assets/images/background.jpg">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
 
 
 <script>
+
     //SMOOTH SCROLLING
-    jQuery('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+
+
 </script>
 
 
